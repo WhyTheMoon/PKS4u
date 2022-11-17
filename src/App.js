afc758044458d37be381f8c2191427919b2d33ab
Shixiang Yang <adam_yang2021@163.com>
2022-11-17 00:37:44 -0800

    some ui fundation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [show, setShow] = useState(false);
+	const showHandler = () => {
+		setShow(true);
+	};
+	const cancelHandler = () => {
+		// console.log("TEST");
+		setShow(false);
+	};
+
+	return (
+		<CartProvider>
+			{show && <Cart onCancelCart={cancelHandler} />}
+			<Header onShowCart={showHandler} />
+			<main>
+				<Meals />
+			</main>
+		</CartProvider>
+	);
 }
 
 export default App;
